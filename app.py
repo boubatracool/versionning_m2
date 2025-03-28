0e6d3ebc6fd8beb8ad7848cd568656f3ff32a6a0
azimari.toure.ikbal <azimari.toure.etu@esmt.sn>
2025-03-28 12:53:34 +0000

    update in frontend and upload route

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, request, render_template, jsonify
 import upload_dataset as upload_dataset
 
 app = Flask(__name__)
@@ -7,14 +7,33 @@ app.secret_key = os.getenv('SECRET_KEY', 'MASTER2IA_VERSIONING2')
 
 
 @app.route("/")
-def hello_world():
-    return render_template('hello.html')
+def index():
+    return render_template('./templates/index.html')
+
 
 @app.route('/upload', methods=['POST'])
 def upload():
     UPLOAD_FOLDER = 'data'
     ALLOWED_EXTENSIONS = {'csv'}
-    return upload_dataset.upload_file(UPLOAD_FOLDER,ALLOWED_EXTENSIONS)
+
+    if 'files' not in request.files:
+        return jsonify({'error': 'No files part in the request'}), 400
+
+    files = request.files.getlist('files')  # Retrieve multiple files
+
+    uploaded_files = []
+    for file in files:
+        if file.filename == '':
+            continue  # Skip empty filenames
+        
+        if file.filename.split('.')[-1].lower() in ALLOWED_EXTENSIONS:
+            file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+            file.save(file_path)
+            uploaded_files.append(file.filename)
+        else:
+            return jsonify({'error': f'File {file.filename} has an invalid extension'}), 400
+
+    return jsonify({'message': 'Files uploaded successfully', 'files': uploaded_files}), 200
 
 
 if __name__ == '__main__':
